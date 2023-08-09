85af342166350af3479fb431329769ed28918386
Thais Spinoza <thais.spinoza@sintese.local>
2023-08-09 09:55:38 -0300

    primeiro endpoint

---
+++ 2TDSPJ.API/Models/Class.cs
@@ -0,0 +1,21 @@
+﻿namespace _2TDSPJ.API.Models
+{
+    public class Class
+    {
+
+
+        public Class()
+        {}
+
+        private int myVar;
+        public int MyProperty
+    {
+        get { return myVar; }
+        set { myVar = value; }
+    }
+
+
+    public int rm { get; set; }
+        public string name { get; set; }
+    }
+}
