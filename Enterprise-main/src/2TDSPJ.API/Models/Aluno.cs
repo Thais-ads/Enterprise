0fb896bcb7bca15788057e7034385761681b65c6
Thais Carvalho Spinoza <101110487+Thais-ads@users.noreply.github.com>
2023-08-14 21:08:24 -0300

    Add files via upload

---
+++ Enterprise-main/src/2TDSPJ.API/Models/Aluno.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace _2TDSPJ.API.Models
+{
+    public class Aluno
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AlunoId { get; set; }
+
+        [Required(ErrorMessage ="Nome Obrigatorio")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = " Obrigatorio")]
+        public bool Ativo { get; set; }
+    }
+}
